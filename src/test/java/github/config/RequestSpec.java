6da9f68591e237883f3b78b7d3404f50e02562cf
Xavier2a2 <abanjoko@spartaglobal.com>
2026-09-12 19:16:56 +0100

    Blacklisted Authorisation from error logs if test failed

--- src/test/java/github/config/RequestSpec.java
+++ src/test/java/github/config/RequestSpec.java
@@ -5,11 +5,17 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.ErrorLoggingFilter;
 import io.restassured.specification.RequestSpecification;
 
+import io.restassured.RestAssured;
+import io.restassured.config.LogConfig;
+
 public class RequestSpec {
     public static RequestSpecification getRequestSpec() {
 
         RequestSpecBuilder builder = new RequestSpecBuilder()
-                .setBaseUri(TestConfig.BASE_URL);
+                .setBaseUri(TestConfig.BASE_URL)
+                .setConfig(RestAssured.config()
+                        .logConfig(
+                                LogConfig.logConfig().blacklistHeader("Authorisation")));
 
         if (TestConfig.GITHUB_TOKEN != null && !TestConfig.GITHUB_TOKEN.isBlank()){
             builder.addHeader(
